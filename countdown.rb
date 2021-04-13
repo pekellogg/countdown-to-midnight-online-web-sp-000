20e0f6a0da8cc231673a66ef74e52033cf22f9a0
pekellogg <pekellogg@hotmail.com>
2021-04-13 12:46:15 -0700

    commit tree bf64e59ebca9d3be3eb55ac5afa644748d8cb5de

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
+require 'pry'
+counter = 10
+def countdown(counter)
+    while counter > 0
+        puts "#{counter} SECOND(S)!"
+        counter -= 1
+    end
+    return "HAPPY NEW YEAR!" if counter == 0
+    # binding.pry
+end
 
-def countdown
+
+def countdown_with_sleep(counter)
+    while counter > 0
+        puts "#{counter} SECOND(S)!"
+        sleep(1)
+        counter -= 1
+    end
+    return "HAPPY NEW YEAR!" if counter == 0
+    # binding.pry
 end
